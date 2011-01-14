088beed90446df2247596a5e43f19b0827c3bdc7
Lee Edwards <leeredwards@gmail.com>
2011-01-13 23:23:05 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'pg'
+gem 'heroku'
+
+gem 'devise'
+gem 'cancan'
+
+group :development, :test do
+	gem 'capybara'
+	gem 'cucumber'
+	gem 'cucumber-rails'
+	gem 'database_cleaner'
+	gem 'jasmine'
+  gem 'rspec'
+	gem 'rspec-rails'
+	gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Activeresume::Application.config.secret_token = 'ec8d29e28d08d97dbc79c461d31c5259c4b11bc5a7410be973ebeb13deb2863109194d72fe6cff44e99af52fdc2ed1fe1aadcaab7a5ab2bf65d4733c857df8bd'
